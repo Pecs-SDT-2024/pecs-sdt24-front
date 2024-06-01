fafeedd4c8431d0bf7bc535d4ee058ede5e7a4dc
frfrankie <frfrankie5@gmail.com>
2024-06-01 16:16:32 +0200

    feat: add recent component

---
+++ src/app/recent/recent.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recent',
+  standalone: true,
+  imports: [],
+  templateUrl: './recent.component.html',
+  styleUrl: './recent.component.css'
+})
+export class RecentComponent {
+
+}
