98763833c355e421b6b058626bcd52c14a9efe2a
Zaymerstone <egor.nesterov2002@gmail.com>
2024-06-01 21:23:21 +0200

    feat: added post component

--- src/app/pages/post/post.component.spec.ts
+++ src/app/pages/post/post.component.spec.ts
@@ -1,23 +1,67 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PostComponent } from './post.component';
+import { HeaderComponent } from '../../layout/header/header.component';
+import { SideComponent } from '../../layout/side/side.component';
+import { RecentComponent } from '../../layout/recent/recent.component';
+import { FooterComponent } from '../../layout/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
-describe('PostComponent', () => {
-  let component: PostComponent;
-  let fixture: ComponentFixture<PostComponent>;
+// Material Styles
+import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import {
+    MAT_DIALOG_DATA,
+    MatDialogModule,
+    MatDialogRef,
+} from '@angular/material/dialog';
+import { of } from 'rxjs/internal/observable/of';
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [PostComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(PostComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+describe('SinglePostComponent', () => {
+    let component: PostComponent;
+    let fixture: ComponentFixture<PostComponent>;
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [
+                PostComponent,
+                HeaderComponent,
+                SideComponent,
+                RecentComponent,
+                FooterComponent,
+            ],
+            imports: [
+                BrowserAnimationsModule,
+                MatDividerModule,
+                MatInputModule,
+                MatIconModule,
+                MatToolbarModule,
+                MatMenuModule,
+                MatDialogModule,
+                HttpClientModule,
+            ],
+            providers: [
+                { provide: MatDialogRef, useValue: {} },
+                { provide: MAT_DIALOG_DATA, useValue: [] },
+                {
+                    provide: ActivatedRoute,
+                    useValue: {
+                        params: of({ id: 0 }),
+                    },
+                },
+            ],
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(PostComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- src/app/pages/post/post.component.ts
+++ src/app/pages/post/post.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ArticlesService } from '../../services/articles.service';
 
 @Component({
-  selector: 'app-post',
-  templateUrl: './post.component.html',
-  styleUrl: './post.component.css'
+    selector: 'app-single-post',
+    templateUrl: './post.component.html',
+    styleUrls: ['./post.component.css'],
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+    post: any;
+    id: number = 0;
+    constructor(
+        private articleService: ArticlesService,
+        private route: ActivatedRoute
+    ) {}
 
+    ngOnInit(): void {
+        // Gettig article id from the url
+        this.route.params.subscribe((param) => {
+            this.id = parseInt(param['id']);
+        });
+
+        this.articleService.getArticle(this.id).subscribe((data) => {
+            this.post = data;
+        });
+    }
 }
