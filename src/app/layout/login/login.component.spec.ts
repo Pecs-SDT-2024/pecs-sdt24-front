dc7b7dbb37364d90d89647b88dc4f68fa06494cc
Zaymerstone <egor.nesterov2002@gmail.com>
2024-06-01 21:25:01 +0200

    feat: added login component

--- src/app/layout/login/login.component.spec.ts
+++ src/app/layout/login/login.component.spec.ts
@@ -1,17 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginComponent } from './login.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-describe('LoginComponent', () => {
+// Material Styles
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
+describe('LoginModalComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent]
+      declarations: [ LoginComponent ],
+      imports: [ MatDialogModule, MatFormFieldModule, MatIconModule, ReactiveFormsModule, MatInputModule, BrowserAnimationsModule],
+      providers: [
+        {provide: MatDialogRef, useValue: {}},
+        {provide: MAT_DIALOG_DATA, useValue: []},
+    ]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/layout/login/login.component.ts
+++ src/app/layout/login/login.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+// Material Styles
+import { MatDialogRef} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+    constructor(
+        public dialogRef: MatDialogRef<LoginComponent>
+    ) {}
 
+    email = new FormControl('', [Validators.required, Validators.email]);
+    hide1= true;
+
+    getErrorMessage() {
+      if (this.email.hasError('required')) {
+        return 'You must enter a value';
+      }
+
+      return this.email.hasError('email') ? 'Not a valid email' : '';
+    }
+
+    onNoClick(): void {
+        this.dialogRef.close();
+    }
+
+    login(): void {
+        console.log("Logging in...")
+        this.dialogRef.close();
+    }
 }
