bff4f2669ea81a1b8ebda80e403010cbc71aa6ed
Zaymerstone <egor.nesterov2002@gmail.com>
2024-06-01 21:25:15 +0200

    feat: added register component

--- src/app/layout/register/register.component.spec.ts
+++ src/app/layout/register/register.component.spec.ts
@@ -1,17 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { RegisterComponent } from './register.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-describe('RegisterComponent', () => {
+// Material Styles
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
+
+describe('RegisterModalComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RegisterComponent]
+      declarations: [ RegisterComponent ],
+      imports: [ MatDialogModule, MatFormFieldModule, MatIconModule, ReactiveFormsModule, MatInputModule, BrowserAnimationsModule],
+      providers: [
+        {provide: MatDialogRef, useValue: {}},
+        {provide: MAT_DIALOG_DATA, useValue: []},
+    ]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/layout/register/register.component.ts
+++ src/app/layout/register/register.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+// Material Styles
+import { MatDialogRef} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
+
 export class RegisterComponent {
 
+    constructor(
+        public dialogRef: MatDialogRef<RegisterComponent>
+    ) {}
+
+    email = new FormControl('', [Validators.required, Validators.email]);
+    hide1= true;
+    hide2= true;
+
+    getErrorMessage() {
+      if (this.email.hasError('required')) {
+        return 'You must enter a value';
+      }
+
+      return this.email.hasError('email') ? 'Not a valid email' : '';
+    }
+
+    onNoClick(): void {
+        this.dialogRef.close();
+    }
+
+    register(): void {
+        console.log("Registering...")
+        this.dialogRef.close();
+    }
 }
