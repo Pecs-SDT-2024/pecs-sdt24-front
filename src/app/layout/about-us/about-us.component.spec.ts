96201bf0d1ec4481f810f69e494065642d1f1f90
Zaymerstone <egor.nesterov2002@gmail.com>
2024-06-01 21:24:31 +0200

    feat: added about-us component

--- src/app/layout/about-us/about-us.component.spec.ts
+++ src/app/layout/about-us/about-us.component.spec.ts
@@ -1,6 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HeaderComponent } from '../../layout/header/header.component';
+import { FooterComponent } from '../../layout/footer/footer.component';
 import { AboutUsComponent } from './about-us.component';
+
+// Material Styles
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('AboutUsComponent', () => {
   let component: AboutUsComponent;
@@ -8,10 +16,21 @@ describe('AboutUsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AboutUsComponent]
+      declarations: [ AboutUsComponent, HeaderComponent, FooterComponent ],
+      imports: [
+        BrowserAnimationsModule,
+        MatIconModule,
+        MatToolbarModule,
+        MatMenuModule,
+        MatDialogModule
+    ],
+    providers: [
+        {provide: MatDialogRef, useValue: {}},
+        {provide: MAT_DIALOG_DATA, useValue: []},
+    ]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(AboutUsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/layout/about-us/about-us.component.ts
+++ src/app/layout/about-us/about-us.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
 
+interface Developer {
+  picture: string;
+  name: string;
+  country: string;
+  position: string;
+  introduction: string;
+}
+
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
-  styleUrl: './about-us.component.css'
+  styleUrls: ['./about-us.component.css']
 })
 export class AboutUsComponent {
-
+  developers: Developer[] = [
+    {
+      picture: 'developer0.jpg',
+      name: 'Egor Nesterov',
+      country: 'Russia',
+      position: 'Frontend Developer',
+      introduction: 'Egor was working as frontend developer using Angular (working with Typscript, HTML, and CSS).'
+    },
+    {
+      picture: 'developer1.jpg',
+      name: 'Kirill Verendeev',
+      country: 'Russia',
+      position: 'Backend Developer',
+      introduction: 'Kirill was working as a backend developer using Laravel (working with PHP).'
+    },
+    {
+      picture: 'developer2.jpg',
+      name: 'Anna Tolmacheva',
+      country: 'Russia',
+      position: 'Project Manager',
+      introduction: 'Anna was working on task creation, the task decomposition, and the design.'
+    },
+    {
+      picture: 'developer3.jpg',
+      name: 'Demirhan Hasirciki',
+      country: 'Turkey',
+      position: 'Database administrator',
+      introduction: 'Demirhan was creating database and tables, their relations'
+    }
+  ];
 }
