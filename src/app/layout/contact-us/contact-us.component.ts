0e03f99b64540a7ef4b79c316551c6a5f8a8d1b3
Zaymerstone <egor.nesterov2002@gmail.com>
2024-06-01 21:24:46 +0200

    feat: added contact-us component

--- src/app/layout/contact-us/contact-us.component.ts
+++ src/app/layout/contact-us/contact-us.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import emailjs from 'emailjs-com';
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
-  styleUrl: './contact-us.component.css'
+  styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent {
+  sendEmail() {
+    const form = document.getElementById('my-contact-form') as HTMLFormElement;
+    emailjs.sendForm('service_yylekov', 'template_bbemuaq', form, 'B-hHq1CL19BH-zPlI')
+      .then((response) => {
+        console.log('Email sent successfully!', response.status, response.text);
+        //send success message
+        alert('Email sent!');
+        form.reset(); // Reset the form input fields
 
+      }, (error) => {
+        console.error('Email sending failed:', error);
+        //send error message
+        alert('Failed to send email. Please try again later.');
+      });
+  }
 }
