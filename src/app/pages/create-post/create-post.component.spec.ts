40cbb0b4b27518b71ba6630741503bbf4e95c03e
Zaymerstone <egor.nesterov2002@gmail.com>
2024-06-01 21:31:54 +0200

    feat: added create-post component

--- src/app/pages/create-post/create-post.component.spec.ts
+++ src/app/pages/create-post/create-post.component.spec.ts
@@ -1,23 +1,53 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from '../../layout/header/header.component';
+import { FooterComponent } from '../../layout/footer/footer.component';
 import { CreatePostComponent } from './create-post.component';
+import {FormsModule} from '@angular/forms';
+// Material Styles
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+    MAT_DIALOG_DATA,
+    MatDialogModule,
+    MatDialogRef,
+} from '@angular/material/dialog';
 
-describe('CreatePostComponent', () => {
-  let component: CreatePostComponent;
-  let fixture: ComponentFixture<CreatePostComponent>;
+describe('WritePostComponent', () => {
+    let component: CreatePostComponent;
+    let fixture: ComponentFixture<CreatePostComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [CreatePostComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(CreatePostComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [
+                CreatePostComponent,
+                HeaderComponent,
+                FooterComponent,
+            ],
+            imports: [
+                HttpClientModule,
+                MatIconModule,
+                MatToolbarModule,
+                MatMenuModule,
+                MatDialogModule,
+                FormsModule,
+                BrowserAnimationsModule
+            ],
+            providers: [
+                { provide: MatDialogRef, useValue: {} },
+                { provide: MAT_DIALOG_DATA, useValue: [] },
+            ],
+        })
+        .compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        fixture = TestBed.createComponent(CreatePostComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- src/app/pages/create-post/create-post.component.ts
+++ src/app/pages/create-post/create-post.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ArticlesService } from '../../services/articles.service';
 
 @Component({
   selector: 'app-create-post',
   templateUrl: './create-post.component.html',
-  styleUrl: './create-post.component.css'
+  styleUrls: ['./create-post.component.css']
 })
-export class CreatePostComponent {
+export class CreatePostComponent implements OnInit {
+    user: string = "Some user";
+    constructor(private articleService : ArticlesService, private router: Router) {}
 
+    ngOnInit(): void {}
+
+    createPost(data:any) {
+        try {
+            let now = new Date()
+            // Passing the data from the html form and adding a user property to it, then posting
+            console.log({...data, Posted: now})
+            return this.articleService.createArticle({...data, Posted: now}).subscribe((result) => {
+                // Go back to some page
+                this.router.navigateByUrl("/")
+            });
+        } catch (err) {
+            console.log(err)
+            return;
+        }
+
+    }
 }
