b9b2430a9f75eb6721e9a50b6abf938e8e5d97df
Zaymerstone <egor.nesterov2002@gmail.com>
2024-06-01 21:26:19 +0200

    feat: added header component

--- src/app/layout/header/header.component.spec.ts
+++ src/app/layout/header/header.component.spec.ts
@@ -1,23 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HeaderComponent } from './header.component';
 
+// Material Styles
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
+
 describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+    let component: HeaderComponent;
+    let fixture: ComponentFixture<HeaderComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [HeaderComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(HeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [HeaderComponent],
+            imports: [MatToolbarModule, MatMenuModule, MatDialogModule],
+            providers: [
+                {provide: MatDialogRef, useValue: {}},
+                {provide: MAT_DIALOG_DATA, useValue: []},
+            ]
+        }).compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        fixture = TestBed.createComponent(HeaderComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { RegisterComponent } from '../register/register.component';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+    constructor(public dialog: MatDialog) {}
 
+    openLoginDialog(): void {
+        this.dialog.open(LoginComponent);
+    }
+
+    openRegisterDialog(): void {
+        this.dialog.open(RegisterComponent);
+    }
 }
