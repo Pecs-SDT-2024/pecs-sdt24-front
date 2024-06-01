f7ea107bb8b4e759ce1619226ec5a385ed5fcd93
frfrankie <frfrankie5@gmail.com>
2024-06-01 19:25:13 +0200

    feat: add create-post, home and post components

---
+++ src/app/create-post/create-post.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-post',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-post.component.html',
+  styleUrl: './create-post.component.css'
+})
+export class CreatePostComponent {
+
+}
